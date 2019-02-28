be432a0c94ffc5340da5803f142a4191ce474290
Jonatan Nadeau <jonatan.nadeau@usherbrooke.ca>
2019-02-28 13:09:35 -0500

    make log directory path a must in config file

--- simulator/src/configurations/generals.rs
+++ simulator/src/configurations/generals.rs
@@ -1,3 +1,4 @@
+use crate::ressources::generals;
 use crate::rng::seed;
 use uuid::Uuid;
 
@@ -15,6 +16,16 @@ impl GeneralConfigurations {
             seed::M_SEED.lock().unwrap().set(uuid);
         } else {
             seed::M_SEED.lock().unwrap().set(Uuid::new_v4());
+        }
+
+        if !self.log_directory.is_empty() {
+            let log_directory = self.log_directory.to_string();
+            generals::M_LOG_DIRECTORY
+                .lock()
+                .unwrap()
+                .replace(log_directory);
+        } else {
+            panic!("Json configuration file must have a log directory path in generals attributs.")
         }
     }
 }
