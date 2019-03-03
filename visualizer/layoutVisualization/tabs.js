f8187fbc138c1c83d067b92ac1d6f76c872a56b6
Gabriel <gabrielbeauchemin@hotmail.fr>
2019-03-02 20:53:50 -0500

    termine la visualisation de heatmap

--- visualizer/layoutVisualization/tabs.js
+++ visualizer/layoutVisualization/tabs.js
@@ -7,14 +7,11 @@
 
 function loadit( element)
 {
-	var visualisationBox = document.getElementById('VisualizationBox');
-	var tabs= document.getElementById('tabs').getElementsByTagName("a");
+	let visualisationBox = document.getElementById('VisualizationBox');
+	let tabs= document.getElementById('tabs').getElementsByTagName("a");
 	for (var i=0; i < tabs.length; i++)
 	{
-		if(tabs[i].rel == element.rel)
-			tabs[i].className="selected";
-		else
-			tabs[i].className="";
+			tabs[i].className =  tabs[i].rel == element.rel ? "selected" : "";
 	}
 
 	updateVisualizationBox();
--- visualizer/layoutVisualization/updateVisualizationBox.js
+++ visualizer/layoutVisualization/updateVisualizationBox.js
@@ -4,16 +4,16 @@ function parseLogs(logs, unitToSelect) {
   logsJson.forEach(function(entry) {
     entry["data"].forEach(function(data) {
       if (data["resolution"] == unitToSelect) {
-				let parsedLog = {
-	        lon: entry["lon"],
-	        lat: entry["lat"],
-	        metricType: entry["metric_type"],
-	        dataType: data["type"],
-	        unit: data["resolution"],
-	        value: data["value"],
-	        interpolation: data["value"] / $("#flat-slider").slider("option", "max") //normalised value
-	      }
-	      parsedLogs.push(parsedLog);
+        let parsedLog = {
+          lon: entry["lon"],
+          lat: entry["lat"],
+          metricType: entry["metric_type"],
+          dataType: data["type"],
+          unit: data["resolution"],
+          value: data["value"],
+          interpolation: data["value"] / $("#flat-slider").slider("option", "max") //normalised value
+        }
+        parsedLogs.push(parsedLog);
       }
     });
   });
@@ -27,10 +27,15 @@ function secToTimestamp(sec) {
   return ("0" + nbrHours).slice(-2) + ":" + ("0" + nbrMinutes).slice(-2) + ":" + ("0" + nbrSeconds).slice(-2);
 }
 
+function switchTabToMap()
+{
+	let visualizationBox = document.getElementById('VisualizationBox');
+	visualizationBox.children[0].style.visibility = 'visible';
+	visualizationBox.children[1].style.visibility = 'hidden';
+}
+
 function updateVisualizationBox() {
   $("body").css("cursor", "wait");
-  let visualizationBox = document.getElementById('VisualizationBox');
-
   let metrics = document.getElementsByName('metricSelection');
   let selectedMetric;
   for (var i = 0, length = metrics.length; i < length; i++) {
@@ -38,32 +43,49 @@ function updateVisualizationBox() {
       selectedMetric = metrics[i];
       break;
     }
-	 }
-
-
-    let mapTab = document.getElementById('tabs').getElementsByTagName("a")[0];
-    let otherTab = document.getElementById('tabs').getElementsByTagName("a")[1];
-
-    let timeValueBegin = $('#flat-slider').slider("option", "values")[0];
-    let timeValueEnd = $('#flat-slider').slider("option", "values")[1];
-
-    if (mapTab.className == "selected") {
-      visualizationBox.children[0].style.visibility = 'visible';
-      visualizationBox.children[1].style.visibility = 'hidden';
-
-      $.ajax({
-        url: "/logs/sherbrooke_sample?min=" + secToTimestamp(timeValueBegin) + "&max=" + secToTimestamp(timeValueEnd),
-        cache: false,
-        success: function(logs) {
-          let parsedLogs = parseLogs(logs,selectedMetric.getAttribute('data-unit'));
-          updateVisualizationLayer(parsedLogs);
-        }
-      });
-
-    } else if (otherTab.className == "selected") {
-      visualizationBox.children[0].style.visibility = 'hidden';
-      visualizationBox.children[1].style.visibility = 'visible';
-      //nothing more to do for now
-    }
-    $("body").css("cursor", "default");
   }
+
+  let coloredPointsTab = document.getElementById('tabs').getElementsByTagName("a")[0];
+  let heatMapTab = document.getElementById('tabs').getElementsByTagName("a")[1];
+
+  let timeValueBegin = $('#flat-slider').slider("option", "values")[0];
+  let timeValueEnd = $('#flat-slider').slider("option", "values")[1];
+
+	if(selectedMetric &&  (timeValueBegin || timeValueBegin === 0)  && (timeValueEnd || timeValueEnd === 0))
+	{
+		let gradient = []
+	  let colors = document.getElementById("legendColors").children;
+	  for (let i = 0, length = colors.length; i < length; i++) {
+	    gradient.push({
+	      r: parseInt(colors[i].getAttribute('data-red')),
+	      g: parseInt(colors[i].getAttribute('data-green')),
+	      b: parseInt(colors[i].getAttribute('data-blue'))
+	    });
+	  }
+
+	  if (coloredPointsTab.className == "selected") {
+	    switchTabToMap();
+	    $.ajax({
+	      url: "/logs/sherbrooke_sample?min=" + secToTimestamp(timeValueBegin) + "&max=" + secToTimestamp(timeValueEnd),
+	      cache: false,
+	      success: function(logs) {
+	        let parsedLogs = parseLogs(logs, selectedMetric.getAttribute('data-unit'));
+	        updateVisualizationLayer(parsedLogs, "coloredPoints", gradient);
+	      }
+	    });
+
+	  } else if (heatMapTab.className == "selected") {
+	    switchTabToMap();
+	    $.ajax({
+	      url: "/logs/sherbrooke_sample?min=" + secToTimestamp(timeValueBegin) + "&max=" + secToTimestamp(timeValueEnd),
+	      cache: false,
+	      success: function(logs) {
+	        let parsedLogs = parseLogs(logs, selectedMetric.getAttribute('data-unit'));
+	        updateVisualizationLayer(parsedLogs, "heatMap", gradient);
+	      }
+	    });
+	  }
+	}
+
+  $("body").css("cursor", "default");
+}
