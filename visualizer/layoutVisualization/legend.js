09d3f26b9574642ac45452aefb93fda9590b2046
Gabriel <gabrielbeauchemin@hotmail.fr>
2019-04-04 19:56:40 -0400

    Ajouter nouveau type de visualisation + corriger path relatif pour pouvoir appeler le script de l'exterieur

--- visualizer/layoutVisualization/legend.js
+++ visualizer/layoutVisualization/legend.js
@@ -1,40 +1,63 @@
-function loadColorGradient(timeLineMin, timeLineMax, gradient)
-{
-	// <div class="gradientbox" style="background-image: linear-gradient(to right,{% for color in legend -%} {{ 'rgb(' + [color.red, color.green, color.blue]|join(',') + ')'}} {{ "," if not loop.last }} {%- endfor %});">
-	// </div>
-	//
-	// <div class="beginLegendBox">
-	// 	{{timeline.min}}
-	// </div>
-	// <div class="middleLegendBox">
-	// 	{{timeline.max/2|round|int}}
-	// </div>
-	// <div class = "endLegendBox">
-	// 	{{timeline.max}}
-	// </div>
-	let gradientBox = document.getElementById("gradientBox");
-	gradientBox.innerHTML = "";
-	let gradientStyle = "background-image: linear-gradient(to right,";
-	for (let i = 0, length = gradient.length; i < length; i++) {
-		gradientStyle += 'rgb(' +
-		                 gradient[i].r.toString() + ',' +
-										 gradient[i].g.toString() + ',' +
-										 gradient[i].b.toString() + ')';
-		if(i < length -1) gradientStyle += ',';
-	};
-	gradientStyle += ')';
+function loadColorGradient(timeLineMin, timeLineMax, gradient) {
+    let legendBoxContent = document.getElementById("legendBoxContent");
+    legendBoxContent.innerHTML = "";
+    let gradientStyle = "background-image: linear-gradient(to right,";
+    for (let i = 0, length = gradient.length; i < length; i++) {
+        gradientStyle += 'rgb(' +
+            gradient[i].r.toString() + ',' +
+            gradient[i].g.toString() + ',' +
+            gradient[i].b.toString() + ')';
+        if (i < length - 1) gradientStyle += ',';
+    }
 
-	//add color gradient
-	gradientBox.innerHTML += `<div class=\"gradientbox\" style=\"${gradientStyle}\">`;
-	//add scalar index
-	let timeLineMiddle = Math.round(timeLineMax / 2.0)
-	gradientBox.innerHTML += `<div class="beginLegendBox">
-														${timeLineMin}
-													</div>
-													<div class="middleLegendBox">
-														${timeLineMiddle}
-													</div>
-													<div class = "endLegendBox">
-														${timeLineMax}
-													</div>`;
+    gradientStyle += ')';
+
+    //add color gradient
+    legendBoxContent.innerHTML += `<div class=\"legendBoxContent\" style=\"${gradientStyle}\">`;
+    //add scalar index
+    let timeLineMiddle = Math.round(timeLineMax / 2.0)
+    legendBoxContent.innerHTML += `<div class="beginLegendBox" style=>
+                                ${timeLineMin}
+                              </div>
+                              <div class="middleLegendBox">
+                                ${timeLineMiddle}
+                              </div>
+                              <div class = "endLegendBox">
+                                ${timeLineMax}
+                              </div>`;
+}
+
+function loadScalablePointsLegend(timeLineMin, timeLineMax, pointMinSize, pointMaxSize, pointColor) {
+    let legendBoxContent = document.getElementById("legendBoxContent");
+    legendBoxContent.innerHTML = "";
+
+    //add sized points
+    let ratioMinMaxPointSize = pointMaxSize / pointMinSize;
+    function getSizePoint(interpolation) { return 3.0 + 3.0 * ratioMinMaxPointSize * interpolation; }
+    let sizeSmallCircle = getSizePoint(0);
+    let sizeMediumCircle = getSizePoint(0.5);
+    let sizeLargeCircle = getSizePoint(1);
+    let circleStyleSmall = `width: ${sizeSmallCircle}px; height: ${sizeSmallCircle}px; border-radius: 50%; background-color: rgba(${pointColor.r},${pointColor.g},${pointColor.b}, ${pointColor.b});`;
+    let circleStyleMedium = `width: ${sizeMediumCircle}px; height: ${sizeMediumCircle}px; border-radius: 50%; background-color: rgba(${pointColor.r},${pointColor.g},${pointColor.b}, ${pointColor.b});`;
+    let circleStyleLarge = `width: ${sizeLargeCircle}px; height: ${sizeLargeCircle}px; border-radius: 50%; background-color: rgba(${pointColor.r},${pointColor.g},${pointColor.b}, ${pointColor.b});`;
+
+    //add scalar index
+    let timeLineMiddle = Math.round(timeLineMax / 2.0)
+
+
+    legendBoxContent.innerHTML += `<div style="display: flex; align-items: center;">  
+                                   <div style="float: left;width: 30%;display: flex;justify-content: center;">  <div style="${circleStyleSmall}"><div style="height:100%; width:100%; font-weight: bold;">${timeLineMin}</div></div> </div> 
+                                   <div style="float: left;width: 30%;display: flex;justify-content: center;">  <div  style="${circleStyleMedium}"><div style="height:100%; width:100%; margin-top:${sizeMediumCircle/2.5}px; font-weight: bold; ">${timeLineMiddle}</div> </div> </div> 
+                                   <div style="float: left;display: flex;justify-content: center;">  <div  style="${circleStyleLarge}"> <div style="height:100%; width:100%; margin-top:${sizeLargeCircle/2.5}px; font-weight: bold;">${timeLineMax}</div> <div> </div>
+                                   </div> </div>`;
+
+     // legendBoxContent.innerHTML += `<div style="clear: both;">  <div style="float: left;width: 17%; text-align: right;">
+     //                               ${timeLineMin}
+     //                               </div>
+     //                               <div style="float:left; width: 30%; text-align: right;">
+     //                               ${timeLineMiddle}
+     //                               </div>
+     //                               <div style="float:left; width: 30%; text-align: right;">
+     //                               ${timeLineMax}
+     //                               </div>  </div>`;
 }
--- visualizer/layoutVisualization/updateVisualizationBox.js
+++ visualizer/layoutVisualization/updateVisualizationBox.js
@@ -1,156 +1,196 @@
 let minMaxByMetric = {};
-function getMinMaxLogInfo(log, metric)
-{
-	if(minMaxByMetric[metric] == undefined)
-	{
-		let logJson = JSON.parse(log);
-		let minValue = Number.POSITIVE_INFINITY;
-		let maxValue = Number.NEGATIVE_INFINITY;
+
+function getMinMaxLogInfo(log, metric) {
+    if (minMaxByMetric[metric] == undefined) {
+        let logJson = JSON.parse(log);
+        let minValue = Number.POSITIVE_INFINITY;
+        let maxValue = Number.NEGATIVE_INFINITY;
         let minTimestamp = Number.POSITIVE_INFINITY;
-		let maxTimestamp = Number.NEGATIVE_INFINITY;
-		logJson.forEach(function(entry) {
+        let maxTimestamp = Number.NEGATIVE_INFINITY;
+        logJson.forEach(function (entry) {
             minTimestamp = Math.min(timestampToSec(entry["timestamp"]), minTimestamp);
             maxTimestamp = Math.max(timestampToSec(entry["timestamp"]), maxTimestamp);
-			entry["data"].forEach(function(data) {
-				minValue = Math.min(data["value"], minValue);
-				maxValue = Math.max(data["value"], maxValue);
-			});
-		});
+            entry["data"].forEach(function (data) {
+                minValue = Math.min(data["value"], minValue);
+                maxValue = Math.max(data["value"], maxValue);
+            });
+        });
         let minMaxInfo = {
-            value: [minValue,maxValue],
-            timestamp: [minTimestamp,maxTimestamp]
+            value: [minValue, maxValue],
+            timestamp: [minTimestamp, maxTimestamp]
         }
-		minMaxByMetric[metric] = minMaxInfo;
-		return minMaxInfo;
-	}
-	else {
-		return minMaxByMetric[metric];
-	}
+        minMaxByMetric[metric] = minMaxInfo;
+        return minMaxInfo;
+    } else {
+        return minMaxByMetric[metric];
+    }
 }
 
 function parseLog(log, unitToSelect, max) {
-  let logJson = JSON.parse(log);
-  let parsedLog = []
-  logJson.forEach(function(entry) {
-    entry["data"].forEach(function(data) {
-      if (data["resolution"] == unitToSelect) {
-        let logEntry = {
-          lon: entry["lon"],
-          lat: entry["lat"],
-          metricType: entry["metric_type"],
-          dataType: data["type"],
-          unit: data["resolution"],
-          value: data["value"],
-          interpolation: data["value"] / max //normalised value
-        }
-        parsedLog.push(logEntry);
-      }
+    let logJson = JSON.parse(log);
+    let parsedLog = []
+    logJson.forEach(function (entry) {
+        entry["data"].forEach(function (data) {
+            if (data["resolution"] == unitToSelect) {
+                let logEntry = {
+                    lon: entry["lon"],
+                    lat: entry["lat"],
+                    metricType: entry["metric_type"],
+                    dataType: data["type"],
+                    unit: data["resolution"],
+                    value: data["value"],
+                    interpolation: data["value"] / max //normalised value
+                }
+                parsedLog.push(logEntry);
+            }
+        });
     });
-  });
-  return parsedLog;
+    return parsedLog;
 }
 
 function secToTimestamp(sec) {
-  let nbrHours = parseInt(sec / 3600.0);
-  let nbrMinutes = parseInt((sec - (nbrHours * 3600.0)) / 60.0);
-  let nbrSeconds = (sec - (nbrHours * 3600.0)) - (nbrMinutes * 60.0)
-  return ("0" + nbrHours).slice(-2) + ":" + ("0" + nbrMinutes).slice(-2) + ":" + ("0" + nbrSeconds).slice(-2);
+    let nbrHours = parseInt(sec / 3600.0);
+    let nbrMinutes = parseInt((sec - (nbrHours * 3600.0)) / 60.0);
+    let nbrSeconds = (sec - (nbrHours * 3600.0)) - (nbrMinutes * 60.0)
+    return ("0" + nbrHours).slice(-2) + ":" + ("0" + nbrMinutes).slice(-2) + ":" + ("0" + nbrSeconds).slice(-2);
 }
 
-function timestampToSec(timestamp)
-{
+function timestampToSec(timestamp) {
     let split = timestamp.split(':');
     return parseInt(split[0]) * 60 * 60 + parseInt(split[1]) * 60 + parseInt(split[2]);
 }
 
 function updateVisualizationBox() {
-  $("body").css("cursor", "wait");
-  let metrics = document.getElementsByName('metricSelection');
-  let selectedMetric;
-  for (var i = 0, length = metrics.length; i < length; i++) {
-    if (metrics[i].checked) {
-      selectedMetric = metrics[i];
-      break;
+    $("body").css("cursor", "wait");
+    let metrics = document.getElementsByName('metricSelection');
+    let selectedMetric;
+    for (var i = 0, length = metrics.length; i < length; i++) {
+        if (metrics[i].checked) {
+            selectedMetric = metrics[i];
+            break;
+        }
     }
-  }
-	if(!selectedMetric)
-	{
-		return;
-	}
-	let logPath = selectedMetric.getAttribute('data-logPath');
-	let logUnit = selectedMetric.getAttribute('data-unit');
+    if (!selectedMetric) {
+        $("body").css("cursor", "default");
+        return;
+    }
+    let logPath = selectedMetric.getAttribute('data-logPath');
+    let logUnit = selectedMetric.getAttribute('data-unit');
 
-  let coloredPointsTab = document.getElementById('tabs').getElementsByTagName("a")[0];
-  let heatMapTab = document.getElementById('tabs').getElementsByTagName("a")[1];
+    let coloredPointsTab = document.getElementById('tabs').getElementsByTagName("a")[0];
+    let heatMapTab = document.getElementById('tabs').getElementsByTagName("a")[1];
+    let ScalablePointsTab = document.getElementById('tabs').getElementsByTagName("a")[2];
 
-	let timeValueBegin = NaN;
-  let timeValueEnd = NaN;
-	let timeValueMin = NaN;
-	let timeValueMax = NaN;
+    let timeValueBegin = NaN;
+    let timeValueEnd = NaN;
+    let timeValueMin = NaN;
+    let timeValueMax = NaN;
 
-  let existTimeline = $('#flat-slider').attr('class') != 'unintialized';
-	if(existTimeline)
-	{
+    let existTimeline = $('#flat-slider').attr('class') != 'unintialized';
+    if (existTimeline) {
         timeValueBegin = $('#flat-slider').slider("option", "values")[0];
-		timeValueEnd = $('#flat-slider').slider("option", "values")[1];
-		timeValueMin = $("#flat-slider").slider("option", "min");
-		timeValueMax = $("#flat-slider").slider("option", "max");
-	}
+        timeValueEnd = $('#flat-slider').slider("option", "values")[1];
+        timeValueMin = $("#flat-slider").slider("option", "min");
+        timeValueMax = $("#flat-slider").slider("option", "max");
+    }
 
-  let gradient = []
-  let colors = document.getElementById("legendColors").children;
-  for (let i = 0, length = colors.length; i < length; i++) {
-    gradient.push({
-      r: parseInt(colors[i].getAttribute('data-red')),
-      g: parseInt(colors[i].getAttribute('data-green')),
-      b: parseInt(colors[i].getAttribute('data-blue'))
-    });
-  }
+    let gradient = []
+    let colors = document.getElementById("legendColors").children;
+    for (let i = 0, length = colors.length; i < length; i++) {
+        gradient.push({
+            r: parseInt(colors[i].getAttribute('data-red')),
+            g: parseInt(colors[i].getAttribute('data-green')),
+            b: parseInt(colors[i].getAttribute('data-blue'))
+        });
+    }
 
-	let urlLog = !existTimeline
-	          ? "/logs?logPath=" + logPath
-			  : "/logs?logPath=" + logPath + "&min=" + secToTimestamp(timeValueBegin) + "&max=" + secToTimestamp(timeValueEnd);
+    let urlLog = !existTimeline
+        ? "/logs?logPath=" + logPath
+        : "/logs?logPath=" + logPath + "&min=" + secToTimestamp(timeValueBegin) + "&max=" + secToTimestamp(timeValueEnd);
 
-  if (coloredPointsTab.className == "selected") {
-    $.ajax({
-      url: urlLog,
-      cache: false,
-      success: function(log) {
-				let minMaxLogInfo = getMinMaxLogInfo(log, coloredPointsTab);
-				let logMinValue = parseInt(minMaxLogInfo.value[0]);
-				let logMaxValue =  Math.ceil(minMaxLogInfo.value[1]);
-                let logMinTimestamp = minMaxLogInfo.timestamp[0];
-                let logMaxTimestamp = minMaxLogInfo.timestamp[1];
-				if(!existTimeline)
-				{
-					updateTimeline(logMinTimestamp, logMaxTimestamp);
-				}
-				loadColorGradient(logMinValue, logMaxValue, gradient);
-				let parsedLog = parseLog(log, logUnit, logMaxValue);
-				updateVisualizationLayer(parsedLog, "coloredPoints", gradient);
-      }
-    });
-
-  } else if (heatMapTab.className == "selected") {
-    $.ajax({
-      url: urlLog,
-      cache: false,
-      success: function(log) {
+    if (coloredPointsTab.className == "selected") {
+        $.ajax({
+            url: urlLog,
+            cache: false,
+            success: function (log) {
                 let minMaxLogInfo = getMinMaxLogInfo(log, coloredPointsTab);
                 let logMinValue = parseInt(minMaxLogInfo.value[0]);
-                let logMaxValue =  Math.ceil(minMaxLogInfo.value[1]);
+                let logMaxValue = Math.ceil(minMaxLogInfo.value[1]);
                 let logMinTimestamp = minMaxLogInfo.timestamp[0];
                 let logMaxTimestamp = minMaxLogInfo.timestamp[1];
-				if(!existTimeline)
-				{
-					updateTimeline(logMinTimestamp, logMaxTimestamp);
-				}
-				loadColorGradient(logMinValue, logMaxValue, gradient);
-				let parsedLog = parseLog(log, logUnit, logMaxValue);
-				updateVisualizationLayer(parsedLog, "heatMap", gradient);
-      }
-    });
-  }
+                if (!existTimeline) {
+                    updateTimeline(logMinTimestamp, logMaxTimestamp);
+                }
+                loadColorGradient(logMinValue, logMaxValue, gradient);
+                let parsedLog = parseLog(log, logUnit, logMaxValue);
+                updateVisualizationLayer(parsedLog, "coloredPoints", gradient);
+            }
+        });
 
-  $("body").css("cursor", "default");
+    } else if (heatMapTab.className == "selected") {
+        $.ajax({
+            url: urlLog,
+            cache: false,
+            success: function (log) {
+                let minMaxLogInfo = getMinMaxLogInfo(log, coloredPointsTab);
+                let logMinValue = parseInt(minMaxLogInfo.value[0]);
+                let logMaxValue = Math.ceil(minMaxLogInfo.value[1]);
+                let logMinTimestamp = minMaxLogInfo.timestamp[0];
+                let logMaxTimestamp = minMaxLogInfo.timestamp[1];
+                if (!existTimeline) {
+                    updateTimeline(logMinTimestamp, logMaxTimestamp);
+                }
+                loadColorGradient(logMinValue, logMaxValue, gradient);
+                let parsedLog = parseLog(log, logUnit, logMaxValue);
+                updateVisualizationLayer(parsedLog, "heatMap", gradient);
+            }
+        });
+    } else if (heatMapTab.className == "selected") {
+        $.ajax({
+            url: urlLog,
+            cache: false,
+            success: function (log) {
+                let minMaxLogInfo = getMinMaxLogInfo(log, coloredPointsTab);
+                let logMinValue = parseInt(minMaxLogInfo.value[0]);
+                let logMaxValue = Math.ceil(minMaxLogInfo.value[1]);
+                let logMinTimestamp = minMaxLogInfo.timestamp[0];
+                let logMaxTimestamp = minMaxLogInfo.timestamp[1];
+                if (!existTimeline) {
+                    updateTimeline(logMinTimestamp, logMaxTimestamp);
+                }
+                loadColorGradient(logMinValue, logMaxValue, gradient);
+                let parsedLog = parseLog(log, logUnit, logMaxValue);
+                updateVisualizationLayer(parsedLog, "heatMap", gradient);
+            }
+        });
+    } else if (ScalablePointsTab.className == "selected") {
+        $.ajax({
+            url: urlLog,
+            cache: false,
+            success: function (log) {
+                let minMaxLogInfo = getMinMaxLogInfo(log, coloredPointsTab);
+                let logMinValue = parseInt(minMaxLogInfo.value[0]);
+                let logMaxValue = Math.ceil(minMaxLogInfo.value[1]);
+                let logMinTimestamp = minMaxLogInfo.timestamp[0];
+                let logMaxTimestamp = minMaxLogInfo.timestamp[1];
+                let pointMinSize = parseInt(document.getElementById("ScalablePointsMin").innerHTML);
+                let pointMaxSize = parseInt(document.getElementById("ScalablePointsMax").innerHTML);
+                if (!existTimeline) {
+                    updateTimeline(logMinTimestamp, logMaxTimestamp);
+                }
+                let colorPointDiv = document.getElementById("ScalablePointsColor");
+                let colorPoint = {
+                    r: parseInt(colorPointDiv.getAttribute('data-red')),
+                    g: parseInt(colorPointDiv.getAttribute('data-green')),
+                    b: parseInt(colorPointDiv.getAttribute('data-blue')),
+                    a: parseInt(colorPointDiv.getAttribute('data-alpha'))
+                };
+                loadScalablePointsLegend(logMinValue, logMaxValue, pointMinSize, pointMaxSize, colorPoint);
+                let parsedLog = parseLog(log, logUnit, logMaxValue);
+                updateVisualizationLayer(parsedLog, "scalablePoints", gradient);
+            }
+        });
+    }
+
+    $("body").css("cursor", "default");
 }
